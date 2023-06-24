7387d308906c683c029ab05107c883fa929fb246
ERA73 <era-73@hotmail.com>
2023-06-24 00:46:34 -0500

    speech each word and the whole sentence

--- en_assistant/src/components/TextToSpeech.jsx
+++ en_assistant/src/components/TextToSpeech.jsx
@@ -1,18 +1,31 @@
+import '../styles/textToSpeech.css'
 import React from 'react';
+import { BsPlayCircle } from 'react-icons/bs';
 
 class TextToSpeech extends React.Component {
-  speak = () => {
-    const { text } = this.props;
-    console.log(text)
+  speak = (text, selected_voice, rate) => {
+    console.log(text, selected_voice, rate)
     const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = window.speechSynthesis.getVoices().find(voice => voice.name === selected_voice);
+    utterance.rate = rate;
     window.speechSynthesis.speak(utterance);
   };
 
   render() {
+    const { text, voice, rate } = this.props;
     return (
-      <button onClick={this.speak}>
-        Reproducir
-      </button>
+      <div className='tts-container'>
+        <div className='tts-sentence'>
+          {text.map(word => (
+            <button className='tts-word' onClick={() => this.speak(word, voice, rate)}>
+              {word}
+            </button>
+          ))}
+        </div>
+        <button onClick={() => this.speak(text.join(" "), voice, rate)}>
+          <BsPlayCircle />
+        </button>
+      </div>
     );
   }
 }
--- en_assistant/src/pages/PhrasalVerbsPage.jsx
+++ en_assistant/src/pages/PhrasalVerbsPage.jsx
@@ -2,16 +2,30 @@ import '../styles/phrasalverbs.css'
 import TextToSpeech from '../components/TextToSpeech'
 
 export function PhrasalVerbsPage() {
-  const experiences = [
-    {"past":["add", "up"], "present":["add", "up"]},
-    {"past":["blow", "up"], "present":["blow", "up"]},
-  ]
-  return (
-    <div className='pv-container'>
-      {experiences.map(data => (
-          <div><TextToSpeech text = {data["past"]}/> <TextToSpeech text = {data["present"]}/></div>
-      ))}
-    </div>
-  )
-  
+    const experiences = [
+        { "past": ["add", "up"], "present": ["add", "up"] },
+        { "past": ["blow", "up"], "present": ["blow", "up"] },
+    ]
+    const en_voices = [
+        'Google US English',
+        'Google UK English Female',
+        'Google UK English Male',
+    ]
+    const es_voices = [
+        'Google español',
+        'Google español de Estados Unidos'
+    ]
+    const voice = en_voices[1];
+    const rate = 0.8;
+    return (
+        <div className='pv-container'>
+            {experiences.map(data => (
+                <div className='pv-row'>
+                    <TextToSpeech text={data["past"]} voice={voice} rate={rate} />
+                    <TextToSpeech text={data["present"]} voice={voice} rate={rate} />
+                </div>
+            ))}
+        </div>
+    )
+
 }
